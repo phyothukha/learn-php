537a644fff452e97f700f6304cb06e2696080b56
phyothukha <phyothukha2193@gmail.com>
2024-06-28 16:14:02 +0630

    fix:add table from product tabel;

--- phyrous_school/student-edit.php
+++ phyrous_school/student-edit.php
@@ -88,8 +88,6 @@
                 <label for="pocket_money" class="block text-sm font-medium mb-2 dark:text-white">Pocket Money</label>
                 <input type="number" value="<?= $row['pocket_money'] ?>" name="pocket_money" id="pocket_money" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Enter Pocket Money">
             </div>
-
-
             <button type="submit" name="calculate" class=" max-w-sm px-5 py-3 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                 Create student
             </button>
